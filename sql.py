60ee0c2520c9a9f114d8d6f0fe07fb48df7cb3f0
Maya Saltzman <mayasaltzman3@gmail.com>
2024-03-10 18:33:40 -0400

    create tables done and working

--- sql.py
+++ sql.py
@@ -19,26 +19,24 @@ class Database:
     def create_tables(self):
         #Customers Table
         self.conn.execute("""CREATE TABLE IF NOT EXISTS Customers 
-                 ( CUSTOMER_ID     INTEGER NOT NULL AUTOINCREMENT,
-                   CUSTOMER_NAME   VARCHAR(MAX) NOT NULL,
-                   CUSTOMER_EMAIL  VARCHAR(MAX) NOT NULL,
-                   CUSTOMER_COUNTRY VARCHAR(3) NOT NULL,
-                   PRIMARY KEY (CUSTOMER_ID) );""" )
-        
-        #Orders Table
-        self.conn.execute("""CREATE TABLE IF NOT EXISTS Orders 
-                 ( ORDER_ID     INTEGER NOT NULL AUTO_INCREMENT,
-                   CUSTOMER_ID   INTEGER NOT NULL,
-                   PRODUCT_CODE  INTEGER NOT NULL,
-                   PRIMARY KEY (ORDER_ID),
-                   FOREIGN KEY (CUSTOMER_ID) REFERENCES Customers,
-                   FORIEGN KEY (PRODUCT_CODE) REFERENCES Products);""" )
+                 ( CUSTOMER_ID     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                   CUSTOMER_NAME   VARCHAR NOT NULL,
+                   CUSTOMER_EMAIL  VARCHAR NOT NULL,
+                   CUSTOMER_COUNTRY VARCHAR(3) NOT NULL );""" )
         
         #Products Table
         self.conn.execute("""CREATE TABLE IF NOT EXISTS Products 
-                 ( PRODUCT_CODE     INTEGER NOT NULL AUTO_INCREMENT,
-                   PRODUCT_NAME   VARCHAR(MAX) NOT NULL,
-                   PRIMARY KEY (PRODUCT_CODE) );""" )
+                 ( PRODUCT_CODE     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                   PRODUCT_NAME   VARCHAR NOT NULL );""" )
+        
+        #Orders Table
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Orders 
+                 ( ORDER_ID     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                   CUSTOMER_ID   INTEGER NOT NULL,
+                   PRODUCT_CODE  INTEGER NOT NULL,
+                   FOREIGN KEY (CUSTOMER_ID) REFERENCES Customers,
+                   FOREIGN KEY (PRODUCT_CODE) REFERENCES Products);""" )
+        
         
         
     #inserting tables
@@ -50,5 +48,6 @@ class Database:
     #data aggregation
     
     #our main for testing sake
-    if __name__ == "__main__":
-        db = 0
+if __name__ == "__main__":
+    db = Database()
+    db.create_tables()
